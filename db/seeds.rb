c3bd53c60e163d818ec19cf44d2fbac664bd697d
ice200117 <ice200117@126.com>
2015-06-30 11:58:47 +0800

    get xian data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,6 @@
 #cities = City.create([{city_name: "安国市", city_name_pinyin: "anguoshi", post_number: 130683, latitude: 38.424, longitude: 115.333 }, { city_name: "清苑县", city_name_pinyin: "qingyuanxian", post_number: 130622, latitude: 38.771, longitude: 115.496}])
 
 cities = City.create( [
-  {city_name: "市辖区", city_name_pinyin: "xianghexian", post_number: 131024, latitude: 39.768, longitude: 117.013}, 
+  {city_name: "市辖区", city_name_pinyin: "shixiaqu", post_number: 131024, latitude: 39.768, longitude: 117.013}, 
   {city_name: "香河县", city_name_pinyin: "xianghexian", post_number: 131024, latitude: 39.768, longitude: 117.013}, 
   {city_name: "清苑县", city_name_pinyin: "qingyuanxian", post_number: 130622, latitude: 38.771, longitude: 115.496}])
