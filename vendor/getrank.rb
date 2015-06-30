8e8d3dead20cbd9d2268161b8b5471e178fe1a67
ice200117 <ice200117@126.com>
2015-06-30 11:13:46 +0800

    get data

--- vendor/getrank.rb
+++ vendor/getrank.rb
@@ -1,4 +1,4 @@
-def get_rank_json(webUrl,secretstr,typestr,datestr='')
+def post_rank_json(webUrl,secretstr,typestr,datestr='')
   begin
     option = {secret:secretstr,type:typestr,date:datestr,key:Digest::MD5.hexdigest(secretstr+typestr+datestr) }
     response = HTTParty.post('http://www.izhenqi.cn/api/getrank.php', :body => option)
@@ -12,4 +12,20 @@ def get_rank_json(webUrl,secretstr,typestr,datestr='')
   end 
 end
 
-get_rank_json('','LANGFANGRANK','DAY')
+
+def get_rank_json(datestr)
+  puts datestr
+  begin
+    response = HTTParty.get('http://115.28.227.231:8082/api/data/day-qxday?date='+datestr)
+    json_data=JSON.parse(response.body)
+    puts json_data
+  rescue
+    puts 'Can not get data from izhenqi, please check network!'
+  end 
+end
+
+#post_rank_json('','LANGFANGRANK','DAY')
+#post_rank_json('','CHINARANK','DAY')
+#post_rank_json('','HEBEIRANK','DAY')
+get_rank_json(Time.new(2014,5,30).strftime("%Y-%m-%d"))
+
