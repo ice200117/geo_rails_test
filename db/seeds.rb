6949a43366af47518775383050b2bd0264e48188
ice200117 <ice200117@126.com>
2015-06-30 11:56:14 +0800

    get xian data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-cities = City.create([{city_name: "安国市", city_name_pinyin: "anguoshi", post_number: 130683, latitude: 38.424, longitude: 115.333 }, { city_name: "清苑县", city_name_pinyin: "qingyuanxian", post_number: 130622, latitude: 38.771, longitude: 115.496}])
+
+#cities = City.create([{city_name: "安国市", city_name_pinyin: "anguoshi", post_number: 130683, latitude: 38.424, longitude: 115.333 }, { city_name: "清苑县", city_name_pinyin: "qingyuanxian", post_number: 130622, latitude: 38.771, longitude: 115.496}])
+
+cities = City.create( [
+  {city_name: "市辖区", city_name_pinyin: "xianghexian", post_number: 131024, latitude: 39.768, longitude: 117.013}, 
+  {city_name: "香河县", city_name_pinyin: "xianghexian", post_number: 131024, latitude: 39.768, longitude: 117.013}, 
+  {city_name: "清苑县", city_name_pinyin: "qingyuanxian", post_number: 130622, latitude: 38.771, longitude: 115.496}])
--- vendor/getrank.rb
+++ vendor/getrank.rb
@@ -13,12 +13,14 @@ def post_rank_json(webUrl,secretstr,typestr,datestr='')
 end
 
 
-def get_rank_json(datestr)
-  puts datestr
+def get_rank_json(date)
+  datestr = date.strftime("%Y-%m-%d")
   begin
     response = HTTParty.get('http://115.28.227.231:8082/api/data/day-qxday?date='+datestr)
     json_data=JSON.parse(response.body)
     puts json_data
+    dc = DayCity.new
+
   rescue
     puts 'Can not get data from izhenqi, please check network!'
   end 
@@ -27,5 +29,13 @@ end
 #post_rank_json('','LANGFANGRANK','DAY')
 #post_rank_json('','CHINARANK','DAY')
 #post_rank_json('','HEBEIRANK','DAY')
-get_rank_json(Time.new(2014,5,30).strftime("%Y-%m-%d"))
+
+oneday = 60*60*24
+stime = Time.new(2014,1,1)
+etime = Time.new(2014,1,2)
+while stime<=etime
+  get_rank_json(stime)
+  puts '==============================================='
+  stime = stime + oneday
+end
 
