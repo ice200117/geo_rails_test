47598061c0f535d9d5549c49d1f1029820399ed5
ice200117 <ice200117@126.com>
2015-07-30 15:27:40 +0800

    baoxi_change_whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,42 +16,12 @@
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
 # end
-every 1.hours do
-	flag=false
-	while flag
-		command "cd /vagrant/geo_rails_test/vendor/getdata && rails r gethourdata.rb -e production"
-		t=TempLfHour.last
-		if t.between?(Time.now.beginning_of_hour - 60*60,Time.now.end_of_hour - 60*60) 
-			flag=true
-		else
-			flag = false
-			sleep 60
-		end
-	end
+every "20 */1 * * * " do
+	command "cd /vagrant/geo_rails_test/vendor/getdata && rails r gethourdata.rb -e production"
 end
 every 1.days,:at=>'1:00 am' do
-	flag=false
-	while flag
-		command "cd /vagrant/geo_rails_test/vendor/getdata && rails r getrancd /vagrant/geo_rails_test/vendor/getdata && rails r getrank.rb"
-		t=TempLfDay.last
-		if t.between?(Time.now.yesterday.beginning_of_day,Time.now.yesterday.end_of_day)
-			flag=true
-		else
-			flag=false
-			sleep 60*60
-		end
-	end
+	command "cd /vagrant/geo_rails_test/vendor/getdata && rails r getrancd /vagrant/geo_rails_test/vendor/getdata && rails r getrank.rb -e production"
 end
 every 1.days,:at=>'1:30 am' do
-	flag=false
-	while flag
-		command "cd /vagrant/geo_rails_test/vendor/getdata && rails r getrancd /vagrant/geo_rails_test/vendor/getdata && rails r get74city_month_year.rb"
-		t=TempLfDay.last
-		if t.between?(Time.now.yesterday.beginning_of_day,Time.now.yesterday.end_of_day)
-			flag=true
-		else
-			flag=false
-			sleep 60*60
-		end
-	end
+	command "cd /vagrant/geo_rails_test/vendor/getdata && rails r getrancd /vagrant/geo_rails_test/vendor/getdata && rails r get74city_month_year.rb -e production"
 end
