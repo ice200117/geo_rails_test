837d46b188bef8f089179684384ef54d524b305b
ice200117 <ice200117@126.com>
2016-05-05 23:02:30 +0800

    add  city_name in diff ann table

--- vendor/rd_hourly_aqi_9km_LF.rb
+++ vendor/rd_hourly_aqi_9km_LF.rb
@@ -5,11 +5,12 @@
 
 def parse_line(line, c)
 	# hc = HourlyCityForecastAirQuality.new
+	c.forecast_real_data.destroy_all
 	sd = line[0,10]
 	delta_hour = line[11,3]
 	sdate = Time.local(sd[0,4],sd[4,2],sd[6,2],sd[8,2])
 	# puts sdate+delta_hour.to_i*3600
-
+	
 	# hc.publish_datetime = sdate
 	# hc.forecast_datetime = sdate+delta_hour.to_i*3600
 	hc = HourlyCityForecastAirQuality.find_or_create_by(city_id: c.id, publish_datetime: sdate, forecast_datetime: sdate+delta_hour.to_i*3600 )
@@ -24,10 +25,20 @@ def parse_line(line, c)
 	hc.NO2 = line[51,6]
 	hc.O3 = line[75,6]
 	hc.VIS = line[32,7]
-
-	# hc.city_id = c.id
 	hc.save
 
+	rc = ForecastRealDatum.find_or_create_by(city_id: c.id, publish_datetime: sdate, forecast_datetime: sdate+delta_hour.to_i*3600 )
+	rc.AQI = line[14,4]
+	rc.main_pol = line[18,13].strip
+	rc.grade = line[31,1]
+	rc.pm25 = line[99,6]
+	rc.pm10 = line[87,6]
+	rc.SO2 = line[39,6]
+	rc.CO = line[63,6]
+	rc.NO2 = line[51,6]
+	rc.O3 = line[75,6]
+	rc.VIS = line[32,7]
+	rc.save
 	#puts '----------'
 	#puts hc.city_id
 	#puts hc.publish_datetime.
