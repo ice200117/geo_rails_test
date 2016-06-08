6a13b330dba442cd276ba9871a040a6354667b1b
Mr_Robot <1526215886@qq.com>
2016-06-08 16:55:32 +0800

    fixed rankchart

--- app/models/temp_sfcities_month.rb
+++ app/models/temp_sfcities_month.rb
@@ -2,21 +2,21 @@ class TempSfcitiesMonth < ActiveRecord::Base
 	validates :city_id, uniqueness: { scope: :data_real_time,
     message: "数据重复！" }
 	belongs_to :city
-	def get_rank_chart_data(name,stime,etime)
+	def self.get_rank_chart_data(name,stime,etime)
 		#获取月数据排名
 		unless Custom::Redis.get("get_rank_chart_data_day")
 			data = Array.new
-			d = City.find_by_city_name_pinyin(name).temp_sfcities_months.where(data_real_time: (stime..etime)).to_a.group_by_month(&:data_real_time).as_json
+			d = City.find_by_city_name(name).temp_sfcities_months.where(data_real_time: (stime..etime)).to_a.group_by_month(&:data_real_time).as_json
 			d.each do |k,v|
 				tmp = v.max_by{|x| x['data_real_time']}
 				if tmp['data_real_time'].to_time.day == tmp['data_real_time'].to_time.end_of_month.day
-					tmp['lastrank'] = 75 - tmp['rank']
+					tmp['lastrank'] = (tmp['rank']==nil) ? nil : (75 - tmp['rank'])
 					tmp['time'] = tmp['data_real_time']
 					tmp['primary_pollutant'] = tmp['main_pol']
 				else
 					tmp['foretime'] = tmp['data_real_time']
 					tmp['forerank'] = tmp['rank']
-					tmp['forelastrank'] = 75 - tmp['rank']
+					tmp['forelastrank'] = (tmp['rank']==nil) ? nil : (75 - tmp['rank'])
 					tmp['forecomplexindex'] = tmp['complexindex']
 					tmp['foreprimary_pollutant'] = tmp['main_pol']
 				end
