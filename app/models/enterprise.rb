35099556923cb9b3bad36aab8ba8ed4372e86c12
Mr_Robot <1526215886@qq.com>
2016-10-07 18:58:26 +0800

    enterprise fixed

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -1,2 +1,6 @@
 class Enterprise < ActiveRecord::Base
+	def get_enterprise_data
+		result=Enterprise.all
+		result
+	end
 end
--- vendor/input_enterprise_data.rb
+++ vendor/input_enterprise_data.rb
@@ -13,20 +13,20 @@ def input
 	file = './vendor/enterprise.xls'
 	deal_excel(file).each do |l|
 		tmp = Hash.new
-        tmp['en_name'] = l[0].strip if !l[0].nil?
-		tmp['latitude'] = l[1] if !l[1].nil?
-		tmp['longitude'] = l[2] if !l[2].nil?
-		tmp['dust_concentration'] = l[3] if !l[3].nil?
-		tmp['dust_convert'] = l[4] if !l[4].nil?
-		tmp['dust_discharge'] = l[5] if !l[5].nil?
-		tmp['so2_concentration'] = l[6] if !l[6].nil?
-		tmp['so2_convert'] = l[7] if !l[7].nil?
-        tmp['so2_discharge'] = l[8] if !l[8].nil?
-        tmp['nox_concentration'] = l[9] if !l[9].nil?
-        tmp['nox_convert'] = l[10] if !l[10].nil?
-		tmp['nox_discharge'] = l[11] if !l[11].nil?
-		tmp['temperature'] = l[12] if !l[12].nil?
-		tmp['discharge_height'] = l[13] if !l[13].nil?
+        tmp['en_name'] = l[0].strip if !l[0].nil?#企业名称
+        tmp['latitude'] = l[1] if !l[1].nil?  #纬度
+		tmp['longitude'] = l[2] if !l[2].nil? #经度
+		tmp['dust_concentration'] = l[3] if !l[3].nil?#烟尘浓度
+		tmp['dust_convert'] = l[4] if !l[4].nil?#	烟尘折算浓度
+		tmp['dust_discharge'] = l[5] if !l[5].nil?#烟尘排放量
+		tmp['so2_concentration'] = l[6] if !l[6].nil?#so2浓度
+		tmp['so2_convert'] = l[7] if !l[7].nil?#so2折算浓度
+        tmp['so2_discharge'] = l[8] if !l[8].nil?#	so2排放量
+        tmp['nox_concentration'] = l[9] if !l[9].nil?#nox浓度
+        tmp['nox_convert'] = l[10] if !l[10].nil?#nox折算浓度
+		tmp['nox_discharge'] = l[11] if !l[11].nil?#nox排放量
+		tmp['temperature'] = l[12] if !l[12].nil?#温度
+		tmp['discharge_height'] = l[13] if !l[13].nil?#排放高度
 		result = Enterprise.create(tmp)
 		if result
 			puts tmp.to_s+' '+'input ok!'
